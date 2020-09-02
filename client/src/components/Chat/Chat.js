4f61892e0c86d1c04a7334aac609ed79a4cb5542
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-01 23:46:02 -0300

    [Client] - Life cycle hooks printing data via console

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+    // Hook alternativa for ComponentDidMount and ComponentUnmont 
+    useEffect(() => {
+        const data = queryString.parse(location.search);
+        
+        console.log(location.search);
+        console.log(data);
+    });
+
     return (
         <h1>Chat</h1>
     )
