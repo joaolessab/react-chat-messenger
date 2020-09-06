79a7cd62f884b773eb495e7f1c635b14c20baf2a
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-06 20:53:51 -0300

    [Client] - Creating InfoBar

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 
 import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
 
 let socket;
 
@@ -57,11 +58,13 @@ const Chat = ({ location }) => {
     return (
         <div className="outerContainer">
             <div className="container">
-                <input 
+                <InfoBar room = {room} />
+                
+                {/*<input 
                     value={message} 
                     onChange={(event) => setMessage(event.target.value)} 
                     onKeyPress = {event => event.key === 'Enter' ? sendMessage(event) : null }
-                />
+                />*/}
             </div>
         </div>
     )
