557ca4143b888890f99bf04283283cf5c2ba2bdf
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-06 21:09:10 -0300

    [Client] - Input component created

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 
@@ -59,12 +60,7 @@ const Chat = ({ location }) => {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room = {room} />
-                
-                {/*<input 
-                    value={message} 
-                    onChange={(event) => setMessage(event.target.value)} 
-                    onKeyPress = {event => event.key === 'Enter' ? sendMessage(event) : null }
-                />*/}
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
     )
