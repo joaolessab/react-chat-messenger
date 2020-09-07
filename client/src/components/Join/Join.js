68b0fcb793012694f5a4ece01fa9cfb8a9161b99
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-07 12:00:01 -0300

    [Client] - Join CSS

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,14 +10,16 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join Chat Messenger</h1>
+                <div className="logo"></div>
+                <h1 className="heading">Chat Messenger</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
                 
                 {/* Link that will call the route only of the parameters are valid */}
                 <Link onClick={ event => (!name || !room) ? event.preventDefault() : null } to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit">Sign In</button>
-                </Link>
+                </Link>                
+                <p className="credit">Developed by João Vitor Lessa</p>
             </div>
         </div>
     )
