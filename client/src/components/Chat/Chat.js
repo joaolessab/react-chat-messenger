7088c5873da417d1a7f7427791bd81294d6508aa
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-09-06 22:26:21 -0300

    [Client] - Refactor

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -13,11 +13,12 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');    
+    const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000';
+    //const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
     // Alternativa Hook for ComponentDidMount and ComponentUnmont 
     useEffect(() => {
@@ -29,52 +30,41 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        { /* Triggerring a function on the server side */ }
         socket.emit('join', { name, room }, (error) => {
             if (error){
                 alert(error);
             }
         });
-
-        return () => {
-            socket.emit('disconnect');
-            socket.off();
-        };
-
-    }, [ENDPOINT, location.search]); { /* According to hooks, only if these two parameters change, then it will trigger the server */ }
-    
+      }, [ENDPOINT, location.search]); { /* According to hooks, only if these two parameters change, then it will trigger the server */ }
+  
     useEffect(() => {
-        socket.on('message', (message) => {
-            setMessages([...messages, message]); // Shortchurt command to add message to the messages array
-        });        
-    
+        socket.on('message', message => {
+          setMessages(messages => [ ...messages, message ]); // Shortchurt command to add message to the messages array
+        });
+        
         socket.on("roomData", ({ users }) => {
-            setUsers(users);
+          setUsers(users);
         });
     }, []);
 
-    // Function for sending messages
     const sendMessage = (event) => {
         event.preventDefault();
 
-        // Send message and clean input
-        if (message){
-            socket.emit('sendMessage', message, () => setMessage(''));
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(''));
         }
-    };
+    }
 
-    console.log(message, messages);
-
-    return (
-        <div className="outerContainer">
-            <div className="container">
-                <InfoBar room = {room} />
-                <Messages messages = {messages} name={name}/>
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-            </div>    
-            <TextContainer users={users}/>
-        </div>
-    )
-};
+  return (
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+    </div>
+  );
+}
 
 export default Chat;
